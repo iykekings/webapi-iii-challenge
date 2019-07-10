b7cd53a0a894efb126232be03c5349d7e7595b4a
EmilyAbrahart <emilyabrahart@gmail.com>
2019-07-10 15:22:54 +0100

    update post filled out, tested and working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -39,7 +39,19 @@ router.delete('/:id', validatePostId, async (req, res) => {
 		});
 });
 
-router.put('/:id', (req, res) => {});
+router.put('/:id', validatePostId, validatePost, async (req, res) => {
+	const id = req.params.id;
+	const postData = req.body;
+	Post.update(id, postData)
+		.then(data => {
+			Post.getById(id).then(data => {
+				res.status(200).json(data);
+			});
+		})
+		.catch(error => {
+			res.status(500).json({ message: 'Unable to update post.' });
+		});
+});
 
 // custom middleware
 
@@ -56,4 +68,13 @@ async function validatePostId(req, res, next) {
 	}
 }
 
+function validatePost(req, res, next) {
+	if (Object.keys(req.body) == 0) {
+		res.status(400).json({ message: 'Missing post data' });
+	} else if (req.body.text) {
+		next();
+	} else {
+		res.status(400).json({ message: 'Missing required text field' });
+	}
+}
 module.exports = router;
