818444d9988672f90999fc6b0943a26bb39a8a52
EmilyAbrahart <emilyabrahart@gmail.com>
2019-07-10 14:56:52 +0100

    validatePostId middleware built out

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,27 @@
-const express = 'express';
+const router = require('express').Router();
+const Post = require('./postDb');
 
-const router = express.Router();
+router.get('/', (req, res) => {});
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {});
 
-});
+router.delete('/:id', (req, res) => {});
 
-router.get('/:id', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put('/:id', (req, res) => {});
 
 // custom middleware
 
-function validatePostId(req, res, next) {
+async function validatePostId(req, res, next) {
+	const id = req.params.id;
 
-};
+	const post = await Post.getById(id);
+
+	if (post) {
+		req.post = post;
+		next();
+	} else {
+		res.status(400).json({ message: 'Invalid post ID' });
+	}
+}
 
 module.exports = router;
