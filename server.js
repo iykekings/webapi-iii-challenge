3767e3f1b62aca19b66e1d7d7b488d9bbeca3487
Ikechukwu <iykekings36@gmail.com>
2019-07-11 08:01:52 +0100

    completed logger function in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = 'express';
 
 const server = express();
+server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +11,21 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  const loggedData = {
+    method: req.method,
+    url: req.url,
+    timestamp: new Date()
+  };
+  console.log(loggedData);
+  next();
 };
 
 module.exports = server;
+
+// const userRouter = require('./users/userRouter');
+// const postRouter = require('./posts/postRouter');
+
+// server.use('/api/users', userRouter);
+// server.use('/api/posts', postRouter);
+
+
