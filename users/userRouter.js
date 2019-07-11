678c8f60bf9886e0092e59e3b6c5b5c52c5664e8
Ikechukwu <iykekings36@gmail.com>
2019-07-11 08:25:25 +0100

    implement get single user

--- users/userRouter.js
+++ users/userRouter.js
@@ -36,7 +36,18 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {});
+router.get('/:id', validateUserId, async (req, res) => {
+  const id = req.params.id;
+  User.getById(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: `Unable to retrieve user with id ${id}` });
+    });
+});
 
 router.get('/:id/posts', (req, res) => {});
 
