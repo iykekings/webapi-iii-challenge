08ed07fe0590ec7f09f30933c9576a2d65b14c0a
Ikechukwu <iykekings36@gmail.com>
2019-07-11 08:33:27 +0100

    implement get all posts

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,26 @@
-const express = 'express';
-
-const router = express.Router();
+const router = require('express').Router();
+const Post = require('./postDb');
 
 router.get('/', (req, res) => {
-
+  Post.get()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Unable to retrieve posts' });
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/', (req, res) => {});
 
-});
+router.get('/:id', (req, res) => {});
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {});
 
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put('/:id', (req, res) => {});
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-
-};
+function validatePostId(req, res, next) {}
 
 module.exports = router;
