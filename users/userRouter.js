0fa24f0f37d41a15c43346973317b7d35e30b898
Ikechukwu <iykekings36@gmail.com>
2019-07-11 08:11:08 +0100

    implement validateUserId

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,36 @@
-const express = 'express';
+const router = require('express').Router();
 
-const router = express.Router();
+router.post('/', (req, res) => {});
 
-router.post('/', (req, res) => {
+router.post('/:id/posts', (req, res) => {});
 
-});
+router.get('/', (req, res) => {});
 
-router.post('/:id/posts', (req, res) => {
+router.get('/:id', (req, res) => {});
 
-});
+router.get('/:id/posts', (req, res) => {});
 
-router.get('/', (req, res) => {
+router.delete('/:id', (req, res) => {});
 
-});
-
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put('/:id', (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
+  const id = req.params.id;
 
-};
+  const user = await User.getById(id);
 
-function validateUser(req, res, next) {
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(400).json({ message: 'Invalid user ID' });
+  }
+}
 
-};
+function validateUser(req, res, next) {}
 
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
